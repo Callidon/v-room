185a5d533760e5221e0ae264bac7651bf19f5419
Thomas Minier <tminier01@gmail.com>
2015-11-24 13:14:22 +0100

    add tests for the Mediator

--- src/main/scala/fr/univnantes/vroom/datacontroller/Mediator.scala
+++ src/main/scala/fr/univnantes/vroom/datacontroller/Mediator.scala
@@ -7,28 +7,25 @@ import fr.univnantes.vroom.core._;
  * Classe représentant un médiateur chargé de rediriger les données vers
  * les classes chargée de leur persistance dans la base de données
  */
-class Mediator {
-
-  private var _adresseManager : DataManager = null;
-  private var _batimentManager : DataManager = null;
-  private var _demandeurManager : DataManager = null;
-  private var _reservationManager : DataManager = null;
-  private var _salleManager : DataManager = null;
+class Mediator(adresseManager : DataManager,
+               batimentManager : DataManager,
+               demandeurManager : DataManager,
+               reservationManager : DataManager,
+               salleManager : DataManager ) {
 
   //TODO remplacer Any par Persistable
   //TODO peut être mettre un type de retour aux fonctions (retourner l'bjet inséré par exemple)
 
   /**
    *
-   * @pre les managers ont été set
    * @param donnee
    */
   def insert(donnee : Any) : Unit = donnee match {
-    case Adresse => _adresseManager.insert(donnee)
-    case Batiment => _batimentManager.insert(donnee)
-    case Demandeur => _demandeurManager.insert(donnee)
-    case Reservation => _reservationManager.insert(donnee)
-    case Salle => _salleManager.insert(donnee)
+    case _ : Adresse => adresseManager.insert(donnee)
+    case _ : Batiment => batimentManager.insert(donnee)
+    case _ : Demandeur => demandeurManager.insert(donnee)
+    case _ : Reservation => reservationManager.insert(donnee)
+    case _ : Salle => salleManager.insert(donnee)
     case _ => //TODO trouver le cas d'exception
   }
 
@@ -37,11 +34,11 @@ class Mediator {
    * @param donnee
    */
   def update(donnee : Any) : Unit = donnee match {
-    case Adresse => _adresseManager.update(donnee)
-    case Batiment => _batimentManager.update(donnee)
-    case Demandeur => _demandeurManager.update(donnee)
-    case Reservation => _reservationManager.update(donnee)
-    case Salle => _salleManager.update(donnee)
+    case _ : Adresse => adresseManager.update(donnee)
+    case _ : Batiment => batimentManager.update(donnee)
+    case _ : Demandeur => demandeurManager.update(donnee)
+    case _ : Reservation => reservationManager.update(donnee)
+    case _ : Salle => salleManager.update(donnee)
     case _ => //TODO trouver le cas d'exception
   }
 
@@ -50,11 +47,11 @@ class Mediator {
    * @param donnee
    */
   def delete(donnee : Any) : Unit = donnee match {
-    case Adresse => _adresseManager.delete(donnee)
-    case Batiment => _batimentManager.delete(donnee)
-    case Demandeur => _demandeurManager.delete(donnee)
-    case Reservation => _reservationManager.delete(donnee)
-    case Salle => _salleManager.delete(donnee)
+    case _ : Adresse => adresseManager.delete(donnee)
+    case _ : Batiment => batimentManager.delete(donnee)
+    case _ : Demandeur => demandeurManager.delete(donnee)
+    case _ : Reservation => reservationManager.delete(donnee)
+    case _ : Salle => salleManager.delete(donnee)
     case _ => //TODO trouver le cas d'exception
   }
 
---
+++ src/test/scala/fr/univnantes/vroom/TestsConstants.scala
@@ -0,0 +1,33 @@
+package fr.univnantes.vroom
+
+import java.util.Date
+
+import fr.univnantes.vroom.core._
+import fr.univnantes.vroom.tarifs.{TarifDuree, TarifManifestation, TarifOrigine, TarifTitre}
+
+/**
+ * Ensemble de constantes pour les tests unitaires
+ */
+object TestsConstants {
+
+  val adresse = new Adresse("5B", "Rue du port", "44000", "Nantes")
+
+  val batiment = new Batiment(10, "Salle des sports", adresse)
+
+  val date = new Date();
+
+  val duree = new TarifDuree(3, "Tarif en fonction de la durée", 60.0)
+
+  val manifestation = new TarifManifestation(4, "Tarif de la manifestation", 15.0)
+
+  val titre = new TarifTitre(5, "Tarif pour le titre A", 40.0)
+
+  val origine = new TarifOrigine(6, "Tarif pour l'origine X", 35.0)
+
+  val demandeur = new Demandeur(10, "Philippe", adresse, origine, titre)
+
+  val salle = new Salle(1, 10, 15, 60, batiment)
+
+  val reservation = new Reservation(2, date, 15.0, salle, demandeur, manifestation, duree)
+
+}
